37f1e12f9b449c86a09b9b003fa6605baa689f82
ArabyanAnna <anna_a@purlin.com>
2023-05-26 10:46:29 +0400

    Sum Of Number Digits

---
+++ SumOfNumberDigits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int digit;
+        Console.WriteLine("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        while (num > 0)
+        {
+            digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+        Console.WriteLine(sum);
+        Console.ReadKey();
+    }
+}
