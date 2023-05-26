23e086d83fb35018664b5fe446fe5e44bf01cb18
ArabyanAnna <anna_a@purlin.com>
2023-05-26 10:42:49 +0400

    Add project files.

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int sum;
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sum = num1 + num2);
+            Console.ReadKey();
+        }
+    }
+}
