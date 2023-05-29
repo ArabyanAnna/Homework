e6b64a0544c60c3f3d33ff14f2277de9f0edf3f2
ArabyanAnna <anna_a@purlin.com>
2023-05-29 15:12:47 +0400

    The integers K and N (N > 0) are given. Multiply the number K N times.

---
+++ ImportNumberAndOutput/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+class Program
+{
+    static void Main()
+
+    {
+        
+
+        Console.WriteLine("Enter a number:");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter a number:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        
+        while (n > 0)
+        {
+            n--;
+            Console.Write(k);
+
+        }
+
+
+       
+        Console.ReadKey();
+    }
+}
--- SumOfTwoNumbers/Program.cs
+++ SumOfTwoNumbers/Program.cs
@@ -4,14 +4,22 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main()
+
         {
-            int sum;
-            Console.WriteLine("Enter the first number:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number:");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(sum = num1 + num2);
-            Console.ReadKey();
+
+            for (int i = 0; i < 1; i++) 
+
+            Console.WriteLine("a");
+
+
+            {
+                for (int l = 0; l < 2; l++)
+
+                    Console.Write("a");
+
+            }
+      
+            
         }
     }
 }
