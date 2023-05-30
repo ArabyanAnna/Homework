fe0d410031ce4df74a079f2734d48716cdb4f3ca
ArabyanAnna <anna_a@purlin.com>
2023-05-30 13:51:34 +0400

    Given A Year, Return The Century It Is In

---
+++ GivenAYearReturnTheCenturyItIsIn/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+class Program
+{
+    static void Main()
+
+    {
+
+        Console.WriteLine("Enter a Year:");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        int century = (year - 1)/ 100 + 1;
+
+        Console.Write(century);
+
+        Console.ReadKey();
+    }
+}
