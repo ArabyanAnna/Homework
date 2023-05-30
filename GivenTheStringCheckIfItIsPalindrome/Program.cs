7da74146f06bb56d0befb9795ee7700e8eff47b5
ArabyanAnna <anna_a@purlin.com>
2023-05-30 14:33:40 +0400

    Given The String Check If It Is Palindrome

---
+++ GivenTheStringCheckIfItIsPalindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+
+    {
+
+        Console.WriteLine("Please, enter a word:");
+        string word = Console.ReadLine();
+
+        char [] charword = word.ToCharArray();
+        Array.Reverse(charword);
+
+        string reverseword = new string(charword); 
+
+        if (word == reverseword) {
+            Console.Write("It is palindrom word");
+        }
+        else Console.Write ("It isn`t a palindrom word");
+
+       
+
+        Console.ReadKey();
+    }
+}
+
