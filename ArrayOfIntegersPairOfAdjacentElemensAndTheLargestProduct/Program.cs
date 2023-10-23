8bb648b81378b33e24db7c986c64516de9cbae62
AnnaArabyan <annaarabyan1984@gmail.com>
2023-10-23 16:43:58 +0400

    Array Of Integers

---
+++ ArrayOfIntegersPairOfAdjacentElemensAndTheLargestProduct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+class Program
+
+
+{
+    static void Main()
+    {
+        int[] myArray = { 3, 6, -2, -5, 7, 3 };
+        int MaxValue  = myArray[0];
+
+        for (int i = 1; i < myArray.Length; i++) {
+            
+              if (myArray[i] > MaxValue) 
+            { 
+                MaxValue = myArray[i];
+            }
+       
+        }
+       
+
+        Console.WriteLine(MaxValue);
+        Console.ReadLine();
+    }
+
+}
