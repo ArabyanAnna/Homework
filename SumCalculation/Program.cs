d134fc1dc834fb444ba2402c1f0ce45519477a66
AnnaArabyan <annaarabyan1984@gmail.com>
2023-10-23 16:44:34 +0400

    Calculate Sum of numbers

---
+++ SumCalculation/Program.cs
@@ -0,0 +1,42 @@
+﻿//Using for loop
+
+int sum = 0;
+for (int i = 0; i < 101; i++)
+
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
+
+
+//Using While loop
+
+int i1 = 0;
+int sum2 = 0;
+
+while (i1 < 100)
+
+{
+    i1++;
+    sum2 = sum2 + i1;
+}
+
+Console.WriteLine(sum2);
+
+
+
+//Using Do While Loop
+
+int sum3 = 0;
+int i3 = 0;
+do
+
+{
+
+    i3++;
+    sum3 = sum3 + i3;
+}
+while (i3 < 100);
+Console.WriteLine(sum3);
+
+
